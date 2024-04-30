90fdf38c16c009386f574e574539a88d1992a7a9
Anak Agung Gde Weida Ksatriawarma <163432329+weida-88@users.noreply.github.com>
2024-04-30 16:32:39 +0800

    Delete crud directory

--- crud/delete_item.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'Item.php';
-
-$item = new Item($pdo);
-$item->deleteItem($_GET['id']);
-header('Location: index.php');
--- crud/view_items.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Item.php';
-
-function view_items($pdo) {
-    $view_item = new Item($pdo);
-    $items = $view_item->getAllItems();
-
-    return $items;
-}
